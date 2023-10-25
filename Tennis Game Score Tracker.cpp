0067030e58cbdc56a7c5d81ed3abe343985e6288
Nitai Satapathy <76805400+nitai-satapathy@users.noreply.github.com>
2023-10-25 13:26:19 +0530

    Add the c programming file

---
+++ Tennis Game Score Tracker.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+// Structure to represent a tennis player
+struct Player {
+    char name[50];
+    int gamesWon;
+    
+};
+
+// Function to display the current game score
+void displayScore(int player1Score, int player2Score) {
+    printf("Current Game Score:\n");
+    printf("Player 1: %d  Player 2: %d\n", player1Score, player2Score);
+}
+
+int main() {
+    // Initialize players
+    struct Player player1, player2;
+    player1.gamesWon = 0;
+    player2.gamesWon = 0;
+
+    printf("Enter Player 1's Name: ");
+    scanf("%s", player1.name);
+    printf("Enter Player 2's Name: ");
+    scanf("%s", player2.name);
+
+    int player1Score = 0, player2Score = 0;
+
+    while (1) {
+        int choice;
+        printf("\nSelect an option:\n");
+        printf("1. Player 1 scores a point\n");
+        printf("2. Player 2 scores a point\n");
+        printf("3. End game\n");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            player1Score++;
+        } else if (choice == 2) {
+            player2Score++;
+        } else if (choice == 3) {
+            // End the current game
+            if (player1Score > player2Score) {
+                player1.gamesWon++;
+                printf("%s wins the game!\n", player1.name);
+            } else if (player2Score > player1Score) {
+                player2.gamesWon++;
+                printf("%s wins the game!\n", player2.name);
+            } else {
+                printf("It's a tie!\n");
+            }
+
+            // Reset scores for the next game
+            player1Score = 0;
+            player2Score = 0;
+
+            // Display player statistics
+            printf("\nPlayer Statistics:\n");
+            printf("%s: %d games won\n", player1.name, player1.gamesWon);
+            printf("%s: %d games won\n", player2.name, player2.gamesWon);
+
+            // Ask if the players want to continue
+            char playAgain;
+            printf("Do you want to play another game? (y/n): ");
+            scanf(" %c", &playAgain);
+
+            if (playAgain != 'y') {
+                printf("Thanks for playing!\n");
+                break; // Exit the program
+            }
+        }
+
+        // Display the current game score
+        displayScore(player1Score, player2Score);
+    }
+
+    return 0;
+}
